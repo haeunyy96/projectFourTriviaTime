aadb6674d88002aa93809ced1c2aa5dc88ab1173
Grace <gracecmccaffrey@gmail.com>
2023-02-09 16:58:52 -0500

    populated Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './index.css';
+import Header from './components/Header';
 import Form from './components/Form';
 
 function App() {
     return (
         <>
+            <Header />
             <Form />
         </>
     )
