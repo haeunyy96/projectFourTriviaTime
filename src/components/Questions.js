8bab46582b240ada105a5116c1eac79cae23a579
Nin Pasker <gooddev.e.nin@gmail.com>
2023-02-09 20:26:47 -0500

    Updating changes

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+
 const Questions = () => {
+    
+    useEffect(() => {
+        const url = new URL('https://opentdb.com/api.php')
+        url.search = new URLSearchParams({
+            amount: 3,
+            category: 10
+        })
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setTriviaQuestions(data)
+            })
+    }, [])
+
     return (
         <>
         </>
