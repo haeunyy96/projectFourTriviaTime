e53bb3347b9202632e1b3a605a29ce16a1d75f2a
Nin Pasker <gooddev.e.nin@gmail.com>
2023-02-09 19:27:07 -0500

    PR funsies

--- src/components/Form.js
+++ src/components/Form.js
@@ -23,10 +23,10 @@ const Form = () => {
                 <label htmlFor="filtration">Ready to Play?</label>
                 <select id="filtration" defaultValue={'placeholder'} onChange={ handlePlayerChange }>
                     <option value="placeholder" disabled>Select amount of players</option>
-                    <option value="1">1 Player</option>
-                    <option value="2">2 Players</option>
-                    <option value="3">3 Players</option>
-                    <option value="4">4 Players</option>
+                    <option value="1">5 Player</option>
+                    <option value="2">6 Players</option>
+                    <option value="3">7 Players</option>
+                    <option value="4">8 Players</option>
                 </select>
                 <button>
                     {
