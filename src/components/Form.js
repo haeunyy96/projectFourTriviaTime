956068a92e26a277d33053bddbb1a17866b1acb6
Grace <gracecmccaffrey@gmail.com>
2023-02-10 20:07:59 -0500

    created form for user quiz category choice and successfully made api call to populate category drop down selection input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import UserChoice from "./UserChoice";
 
 const Form = () => {    
 
     const [ numberOfPlayers, setNumberOfPlayers ] = useState('');
-
+    const [quizCategories, setQuizCategories] = useState([]);
     const [isVisible, setIsVisible] = useState(false);
 
     const handlePlayerChange = (event) => {
@@ -17,32 +17,65 @@ const Form = () => {
         setIsVisible(!isVisible);
     }
 
-    return (
-        <section className='playerChoiceForm'>
-            <form action="" onSubmit={(event) => handleNumberOfPlayersSubmit(event, numberOfPlayers)}>
-                <label htmlFor="filtration">Ready to Play?</label>
-                <select id="filtration" defaultValue={'placeholder'} onChange={ handlePlayerChange }>
-                    <option value="placeholder" disabled>Select amount of players</option>
-                    <option value="1">1 Player</option>
-                    <option value="2">2 Players</option>
-                    <option value="3">3 Players</option>
-                    <option value="4">4 Players</option>
-                </select>
-                <button>
-                    {
-                        isVisible
-                            ? 'Nevermind'
-                            : 'Let\'s play'
-                    }
-                </button>
-            </form>
-            {
-                isVisible
-                    ? <UserChoice numOfPlayers={numberOfPlayers} />
-                    : null
-            }
-        </section>
+    //api call to populate drop down options for categories
+    useEffect(() => {
+        const url = new URL('https://opentdb.com/api_category.php')
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setQuizCategories(data.trivia_categories);
+            })
+    }, [])
 
+    return (
+        <section>
+            <div className='playerChoiceForm'>
+                <form action="" onSubmit={(event) => handleNumberOfPlayersSubmit(event, numberOfPlayers)}>
+                    <label htmlFor="filtration">Ready to Play?</label>
+                    <select id="filtration" defaultValue={'placeholder'} onChange={ handlePlayerChange }>
+                        <option value="placeholder" disabled>Select amount of players</option>
+                        <option value="1">1 Player</option>
+                        <option value="2">2 Players</option>
+                        <option value="3">3 Players</option>
+                        <option value="4">4 Players</option>
+                    </select>
+                    <button>
+                        {
+                            isVisible
+                                ? 'Nevermind'
+                                : 'Let\'s play'
+                        }
+                    </button>
+                </form>
+                {
+                    isVisible
+                        ? <UserChoice numOfPlayers={numberOfPlayers} />
+                        : null
+                }
+            </div>
+            <div className="categoryChoiceForm">
+                <form action="">
+                    <label htmlFor="categoryChoice">Choose a Quiz Category</label>
+                    <select id="categoryChoice" defaultValue={'placeholder'}>
+                        <option value="placeholder" disabled>Select Category</option>
+                        {
+                            quizCategories.map((quizCategory)=>{
+                                return <option key={quizCategory.id} value={quizCategory.id}>{quizCategory.name}</option>
+                            })
+                        }
+                    </select>
+                    <button>
+                        {
+                            isVisible
+                                ? 'Nevermind'
+                                : 'Let\'s play'
+                        }
+                    </button>
+                </form>
+            </div>
+        </section>
     )
 }
 
