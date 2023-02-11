e5b74bd6f2e1445ede16a649ec12f1aaf3dd27d8
Grace <gracecmccaffrey@gmail.com>
2023-02-10 22:19:06 -0500

    tested functionality of arrayOfQuestions props in Question component by creating temporary ul and mapping through array

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,3 @@
-import { number } from "prop-types";
 import { useState, useEffect } from "react";
 import Questions from "./Questions";
 import UserChoice from "./UserChoice";
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,10 +1,19 @@
 const Questions = ({arrayOfQuestions}) => {
 
-    console.log(arrayOfQuestions)
+    // console.log(arrayOfQuestions)
 
     return (
         <>
         <p>test! this is where the questions go!</p>
+            <ul className="listOfQuestions">
+                {
+                    arrayOfQuestions.map((question, index) => {
+                        return <li key={index}>
+                            <p>{question.question}</p>
+                        </li>
+                    })
+                }
+            </ul>
         </>
     )
 }
