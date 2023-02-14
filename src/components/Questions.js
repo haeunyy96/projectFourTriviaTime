dfaa26277b6637dc1e7ba60fa1e29a4e2198cedd
James <jam3sstotomas@gmail.com>
2023-02-14 01:18:16 -0500

    added rough draft of logic for trivia gameplay.

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,23 +1,57 @@
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import firebase from './firebase';
 
 const Questions = () => {
 
+    const [questionIndex, setQuestionIndex] = useState(0);
+    // const [correctAnswer, setCorrectAnswer] = useState('');
+    // const [incorrectAnswer, setIncorrectAnswer] = useState('');
+    // const [answersArray, setAnswersArray] = useState([])
+    const [userAnswer, setUserAnswer] = useState('')
+
     const location = useLocation();
-    console.log(location)
+    const triviaQuestions = location.state //trivia question array from api
+
+    const answersArray = [] //empty array to store all answers
+    const correctAnswer = decodeURIComponent(triviaQuestions[questionIndex].correct_answer) //variable for correct answer - move to state
+    const incorrectAnswer = triviaQuestions[questionIndex].incorrect_answers //variable for incorrect answers array - also move to state?
+    
+    //function to display question with questionIndex variable
+    const displayQuestion = ( )=> {
+        return <p>{decodeURIComponent(triviaQuestions[questionIndex].question)}</p>
+    }
+
+    //function to push correct answer, map through incorrect answer array and push into same array
+    const addToAnswersArray = () => {
+        answersArray.push(correctAnswer)
+
+        incorrectAnswer.map((answer) => {
+            answersArray.push(decodeURIComponent(answer))
+        })
+    }
+
+    //event handler to save users answer to state
+    const handleClick = (e) => {
+        setUserAnswer(e.target.value)
+    }
+
+    //event handler to check if users answer is right and change index number in displayQuestion function so it will display next question in triviaQuestions array
+    const submitAnswer = () => {
+        return userAnswer === correctAnswer
+        ? setQuestionIndex(questionIndex + 1) //eventually add a function to add points for current player
+        : alert('Incorrect. Please try again')
+    }
 
     return (
         <>
-        <p>test! this is where the questions go! </p>
-            <ul className="listOfQuestions">
-                {
-                    location.state.map((question, index) => {
-                        return <li key={index}>
-                            <p>{decodeURIComponent(question.question)}</p>
-                        </li>
-                    })
-                }
-            </ul>
+            {displayQuestion()}
+            {addToAnswersArray()}
+            
+            {answersArray.map((answer, index) => {
+                return <label htmlFor={answer} key={index}>{answer}<input type="radio" name="trivia" id="answer" value={answer} onClick={handleClick} /></label>
+            })}
+            <button onClick={submitAnswer}>Submit</button>
         </>
     )
 }
